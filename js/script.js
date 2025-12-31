a1c269327927f46f68a13d80364dbbad1edae247
Harsh-09A <harsh.autowebbed@gmail.com>
2025-12-31 10:49:50 +0530

    Working Sticky Header

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const header = document.getElementById("middleHeader");
-const headerOffset = header.offsetTop;
+const headerOffset = header.offsetTop - 85;
 
 window.addEventListener("scroll", () => {
   if (window.pageYOffset > headerOffset) {
-    header.classList.add("sticky-active");
+    header.classList.add("sticky-activate");
   } else {
-    header.classList.remove("sticky-active");
+    header.classList.remove("sticky-activate");
   }
 });
