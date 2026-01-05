722e2011ffa513d656591e5a96245cff9cf19fb0
Harsh-09A <harsh.autowebbed@gmail.com>
2026-01-05 12:20:39 +0530

    Home Page Structure Done

--- js/script.js
+++ js/script.js
@@ -8,3 +8,36 @@ window.addEventListener("scroll", () => {
     header.classList.remove("sticky-activate");
   }
 });
+
+
+// document.addEventListener("scroll", function () {
+//     const header = document.getElementById("header");
+//     const nav = document.getElementById("header-nav");
+
+//     if (header.classList.contains("sticky") && header.classList.contains("sticky-active")) {
+//         nav.classList.add("bg-white");
+//     } else {
+//         nav.classList.remove("bg-white");
+//     }
+// });
+
+
+// document.addEventListener("scroll", function () {
+//     const header = document.getElementById("header");
+//     const nav = document.getElementById("header-nav");
+
+//     if (!header || !nav) return;
+
+//     // Desktop only (Bootstrap lg and above)
+//     if (window.innerWidth < 992) {
+//         nav.classList.remove("bg-white"); // reset on mobile
+//         return;
+//     }
+
+//     if (header.classList.contains("sticky") && header.classList.contains("sticky-active")) {
+//         nav.classList.add("bg-white");
+//     } else {
+//         nav.classList.remove("bg-white");
+//     }
+// });
+
